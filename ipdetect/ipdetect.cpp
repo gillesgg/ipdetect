f3e226db97269795c7f2c3b51d74741a0ac3f12a
Gilles Guimard <gillesg@microsoft.com>
2020-04-20 17:17:45 +0200

    implement ipv6 detection

--- ipdetect/ipdetect.cpp
+++ ipdetect/ipdetect.cpp
@@ -2,8 +2,47 @@
 //
 
 #include <iostream>
+#include <ws2spi.h>
+#include <windows.h>
+#include <vector>
+
+bool supportIPv6() 
+{
+    INT iNuminfo = 0;
+    DWORD bufferSize = 0;
+    std::vector<BYTE> protocolInfo;
+    INT iErrno = NO_ERROR;
+    LPWSAPROTOCOL_INFOW lpProtocolInfo = nullptr;
+
+    while ((iNuminfo = WSCEnumProtocols(nullptr, lpProtocolInfo, &bufferSize,&iErrno)) == SOCKET_ERROR) 
+    {
+        if (iErrno == WSAENOBUFS) {
+            protocolInfo.resize(bufferSize, 0);
+            lpProtocolInfo = reinterpret_cast<LPWSAPROTOCOL_INFOW>(protocolInfo.data());
+        }
+        else 
+        {
+            std::cerr << "WSCEnumProtocols failed with error: " << iErrno << std::endl;
+            WSACleanup();
+            exit(1);
+        }
+    }
+
+    for (INT i = 0; i < iNuminfo; ++i) 
+    {       
+        std::wcout << R"(protocol=)" << lpProtocolInfo[i].szProtocol << std::endl;
+
+        if (lpProtocolInfo[i].iAddressFamily == AF_INET6) return true;
+    }
+    return false;
+}
+
+
 
 int main()
 {
-
+    if (supportIPv6() == true)
+    {
+        std::cout << "IPv6 is supported" << std::endl;
+    }
 }
